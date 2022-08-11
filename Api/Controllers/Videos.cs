8898b7c2f7b9228bc5e19e5e36083c7050f0e8b9
Cris Wilson <cris.wilson@criss-mbp.lan>
2022-08-11 15:45:36 +0100

    Text is already json

--- Api/Controllers/Videos.cs
+++ Api/Controllers/Videos.cs
@@ -10,7 +10,6 @@ public class VideosController : ControllerBase
     [HttpGet(Name = "GetVideos")]
     public object Get()
     {
-        string text = System.IO.File.ReadAllText(@"../Test.Data.Files/videos.json");
-        return JsonSerializer.Serialize(text);
+        return System.IO.File.ReadAllText(@"../Test.Data.Files/videos.json");
     }
 }
