b5bea3bc65d7f22deb9f37f9c7f589bc4efbd79e
mcoffey-mt <matthew.coffey@madetech.com>
2022-08-11 09:21:15 +0100

    - Added CustomerTests and VideoTests classes to populate with some unit tests - Added a test case to demonstrate unit test within VideoTests.cs

---
+++ Api.Tests/CustomerTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Api.Models;
+
+namespace Api.Tests;
+
+[TestClass]
+public class CustomerTests
+{
+    [TestMethod]
+    public void IsCreateSuccessful()
+    {
+
+    }
+}
