af59987be460b93207a0033c6f387444a0fe83bb
mcoffey-mt <matthew.coffey@madetech.com>
2022-08-24 15:42:28 +0100

    Altered healthcheck message. Removed IsDevelopment constraint for Swagger.

--- Api/Program.cs
+++ Api/Program.cs
@@ -13,11 +13,8 @@ builder.Services.AddSingleton<IDatabaseController, MongoController>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
